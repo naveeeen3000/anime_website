e24dee9a9daeb2440d918f41de16e1c52e5c2536
naveeeeeeen <naveen.s1@practo.com>
2022-03-29 12:54:13 +0530

    feat. added: search bar

--- static/js/search.js
+++ static/js/search.js
@@ -1,27 +1,41 @@
-console.log("IN THE JS")
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  
 
-function showResult(val){
+async function showResult(val){
     console.log("runnig//")
+
     res=document.getElementById("result")
+    // res=document.createElement("div")
     res.innerHTML=''
     if(val==''){
     return;
     }
 
     let list=''
-    fetch("https://kitsu.io/api/edge/anime/?filter[text]="+val).then(
+    await sleep(2000);
+    setTimeout(fetch("https://kitsu.io/api/edge/anime/?filter[text]="+val).then(
         function(response){
             return response.json();
         }
     ).then(function(data){
+        console.log(data)
+        data=data['data']
+        console.log()
         for(i=0;i<data.length;i++){
-            list+='<li>'+data[i]+ '</li>'
+
+            // list+='<li><h6>'+data[i]['attributes']['canonicalTitle']+ '</h6></li>'
+            list+="<img src="+data[i]['attributes']['posterImage']['tiny']+" >"
         }
-        res.innerHTML='<ul>' +list +'</ul>'
+        console.log('RESULT')
+        console.log(list)
+        res.innerHTML="<ul class='list-item'>" +list +'</ul>'
         return true
     }).catch(function(err){
         console.warn("something went wring ",err)
         return  false
-    })
+    }),2000)
 
 }
